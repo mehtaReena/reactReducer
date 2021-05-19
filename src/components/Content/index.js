7afd00764dd95d01c1fc49ff90829b35789cf31f
mehtaReena <j.reena07@gmail.com>
2021-05-19 23:05:59 +0530

    commit

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -7,7 +7,71 @@ import Counter from '../Counter';
 export default function Content(props){
     return(
         <div className='content'>
-        <Counter></Counter>
+            <div>
+
+            <Counter></Counter>
+            </div>
+
+          <div>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+
+
+
+          </div>
+
         </div>
     )
 
