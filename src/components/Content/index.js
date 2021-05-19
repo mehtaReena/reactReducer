f7efae61246906daefde264beef2600b86fa96a6
mehtaReena <j.reena07@gmail.com>
2021-05-19 23:08:00 +0530

    commit

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -67,6 +67,24 @@ export default function Content(props){
               explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
                Voluptatum ducimus voluptates voluptas?
               </p>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis
+               exercitationem culpa nesciunt nihil aut nostrum
+              explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+               Voluptatum ducimus voluptates voluptas?
+              </p>
 
 
 
